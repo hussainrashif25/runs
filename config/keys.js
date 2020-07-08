775abcd2677ce1ecefe65bb7d51344169d02f7d0
hussainrashif25 <hussainrashif25@gmail.com>
2020-07-07 21:13:24 -0400

    security vunerablity

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://admin:852951753@$$@runscluster-mpt3p.mongodb.net/user?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://admin:k88r4uxzUnE48gEc@runscluster-mpt3p.mongodb.net/user?retryWrites=true&w=majority",
     secretOrKey: "secret"
 }
