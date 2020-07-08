83d39b43058a89894f00357ddba1a181db13d207
hussainrashif25 <hussainrashif25@gmail.com>
2020-07-07 22:25:40 -0400

    C:/Program Files/Git/create works with postman

--- models/Game.js
+++ models/Game.js
@@ -2,10 +2,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const GameSchema = new Schema ({
-    name: {
-        type: String,
-        required: true
-    },
     owner: {
         type: Object,
         required: true
